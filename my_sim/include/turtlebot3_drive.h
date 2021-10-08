48c4fe12d21b32b2c6ba3a5a03932e5c671457d4
JamesGitHub50 <jcal3793@uni.sydney.edu.au>
2021-10-08 17:05:32 +1100

    Redesigned to be object oriented; with the objects: Turtlebot3, COdon, CLaser

--- my_sim/include/turtlebot3_drive.h
+++ my_sim/include/turtlebot3_drive.h
@@ -14,6 +14,22 @@
 * limitations under the License.
 *******************************************************************************/
 
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 /* Authors: Taehun Lim (Darby) */
 
 #ifndef TURTLEBOT3_DRIVE_H_
@@ -27,56 +43,88 @@
 
 #define DEG2RAD (M_PI / 180.0)
 #define RAD2DEG (180.0 / M_PI)
+#define Max_detected 20
 
-#define CENTER 0
-#define LEFT   1
-#define RIGHT  2
+#define CENTER    0
+#define LEFT_15   1
+#define LEFT_30   2
+#define LEFT_45  3
+#define LEFT_60  4
+#define LEFT_75  5
+#define LEFT_90  6
+#define FOR_BACK 7
+#define RIGHT_270  8
+#define RIGHT_285  9
+#define RIGHT_300  10
+#define RIGHT_315  11
+#define RIGHT_330  12
+#define RIGHT_345  13
 
-#define LINEAR_VELOCITY  0.3
-#define ANGULAR_VELOCITY 1.5
+#define LINEAR_VELOCITY  0.2   // 0.1
+#define ANGULAR_VELOCITY 1.3   //1.3
 
 #define GET_TB3_DIRECTION 0
 #define TB3_DRIVE_FORWARD 1
 #define TB3_RIGHT_TURN    2
 #define TB3_LEFT_TURN     3
 
-class Turtlebot3Drive
+class CLaser
 {
  public:
-  Turtlebot3Drive();
-  ~Turtlebot3Drive();
+  //Laser callback
+  void laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg);
+  //Initialise laser 
+  void initLaser(ros::NodeHandle *nh_);
+  //Datapoints scanned by laser
+  double scan_data_[13] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+ private:
+  //ROS topic laser subscriber
+  ros::Subscriber laser_scan_sub_;
+  
+};
+
+class COdom
+{
+ public:
+  //Initialise function
+  void initOdom(ros::NodeHandle *nh_);
+  //Odom callback  
+  void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg);
+
+  //Current and previous position
+  double tb3_pose_;
+  double prev_tb3_pose_;
+ private:
+  //ROS topic Odom subscriber
+  ros::Subscriber odom_sub_;
+  
+};
+
+class Turtlebot3
+{
+ public:
+  Turtlebot3();
+  ~Turtlebot3();
   bool init();
   bool controlLoop();
 
  private:
-  // ROS NodeHandle
+  CLaser Laser;
+  COdom Odom;
+
+  //ROS NodeHandle
   ros::NodeHandle nh_;
   ros::NodeHandle nh_priv_;
 
-  // ROS Parameters
-
-  // ROS Time
-
-  // ROS Topic Publishers
+  //ROS Topic Publishers
   ros::Publisher cmd_vel_pub_;
 
-  // ROS Topic Subscribers
-  ros::Subscriber laser_scan_sub_;
-  ros::Subscriber odom_sub_;
-
-  // Variables
+  //Variables
   double escape_range_;
   double check_forward_dist_;
   double check_side_dist_;
 
-  double scan_data_[3] = {0.0, 0.0, 0.0};
-
-  double tb3_pose_;
-  double prev_tb3_pose_;
-
-  // Function prototypes
+  //Function prototypes
   void updatecommandVelocity(double linear, double angular);
-  void laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg);
-  void odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg);
 };
 #endif // TURTLEBOT3_DRIVE_H_
--- my_sim/src/turtlebot3_drive.cpp
+++ my_sim/src/turtlebot3_drive.cpp
@@ -18,7 +18,7 @@
 
 #include "turtlebot3_gazebo/turtlebot3_drive.h"
 
-Turtlebot3Drive::Turtlebot3Drive()
+Turtlebot3::Turtlebot3()
   : nh_priv_("~")
 {
   //Init gazebo ros turtlebot3 node
@@ -27,7 +27,7 @@ Turtlebot3Drive::Turtlebot3Drive()
   ROS_ASSERT(ret);
 }
 
-Turtlebot3Drive::~Turtlebot3Drive()
+Turtlebot3::~Turtlebot3()
 {
   updatecommandVelocity(0.0, 0.0);
   ros::shutdown();
@@ -36,47 +36,62 @@ Turtlebot3Drive::~Turtlebot3Drive()
 /*******************************************************************************
 * Init function
 *******************************************************************************/
-bool Turtlebot3Drive::init()
+void CLaser::initLaser(ros::NodeHandle *nh_){
+  //Laser subscribe 
+  laser_scan_sub_  = nh_->subscribe("scan", 10, &CLaser::laserScanMsgCallBack, this);
+}
+
+void COdom::initOdom(ros::NodeHandle *nh_){
+  //Initialise current and prev positions
+  tb3_pose_ = 0.0;
+  prev_tb3_pose_ = 0.0;
+  //Odom subscribe
+  odom_sub_ = nh_->subscribe("odom", 10, &COdom::odomMsgCallBack, this);
+}
+
+bool Turtlebot3::init()
 {
   // initialize ROS parameter
   std::string cmd_vel_topic_name = nh_.param<std::string>("cmd_vel_topic_name", "");
 
   // initialize variables
   escape_range_       = 30.0 * DEG2RAD;
-  check_forward_dist_ = 0.7;
-  check_side_dist_    = 0.6;
-
-  tb3_pose_ = 0.0;
-  prev_tb3_pose_ = 0.0;
+  check_forward_dist_ = 0.5;
+  check_side_dist_    = 0.55; //0.5
 
   // initialize publishers
   cmd_vel_pub_   = nh_.advertise<geometry_msgs::Twist>(cmd_vel_topic_name, 10);
 
-  // initialize subscribers
-  laser_scan_sub_  = nh_.subscribe("scan", 10, &Turtlebot3Drive::laserScanMsgCallBack, this);
-  odom_sub_ = nh_.subscribe("odom", 10, &Turtlebot3Drive::odomMsgCallBack, this);
+  //Intialise laser
+  Laser.initLaser(&nh_);
+  //Intialise odom
+  Odom.initOdom(&nh_);
 
   return true;
 }
 
-void Turtlebot3Drive::odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg)
+void COdom::odomMsgCallBack(const nav_msgs::Odometry::ConstPtr &msg)
 {
   double siny = 2.0 * (msg->pose.pose.orientation.w * msg->pose.pose.orientation.z + msg->pose.pose.orientation.x * msg->pose.pose.orientation.y);
 	double cosy = 1.0 - 2.0 * (msg->pose.pose.orientation.y * msg->pose.pose.orientation.y + msg->pose.pose.orientation.z * msg->pose.pose.orientation.z);  
-
+        
+        //Calculate current position
 	tb3_pose_ = atan2(siny, cosy);
 }
 
-void Turtlebot3Drive::laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg)
+void CLaser::laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
-  uint16_t scan_angle[3] = {0, 30, 330};
+  uint16_t scan_angle[14] = {0,15,30,45,60,75,90,180,270,285,300,315,330,345};
 
-  for (int num = 0; num < 3; num++)
+  //Scan location at different angles
+  for (int num = 0; num < 14; num++)
   {
+    //If data is infinity, then let it equal max range
     if (std::isinf(msg->ranges.at(scan_angle[num])))
     {
       scan_data_[num] = msg->range_max;
     }
+    //Store data
     else
     {
       scan_data_[num] = msg->ranges.at(scan_angle[num]);
@@ -84,10 +99,11 @@ void Turtlebot3Drive::laserScanMsgCallBack(const sensor_msgs::LaserScan::ConstPt
   }
 }
 
-void Turtlebot3Drive::updatecommandVelocity(double linear, double angular)
+void Turtlebot3::updatecommandVelocity(double linear, double angular)
 {
   geometry_msgs::Twist cmd_vel;
 
+  //Update linear and angular vel
   cmd_vel.linear.x  = linear;
   cmd_vel.angular.z = angular;
 
@@ -97,54 +113,55 @@ void Turtlebot3Drive::updatecommandVelocity(double linear, double angular)
 /*******************************************************************************
 * Control Loop function
 *******************************************************************************/
-bool Turtlebot3Drive::controlLoop()
+bool Turtlebot3::controlLoop()
 {
+  //Initialise the turtlebot's state
   static uint8_t turtlebot3_state_num = 0;
-
+  //static bool FLAG =true;
   switch(turtlebot3_state_num)
   {
+    //Determine which direction robot needs to move
     case GET_TB3_DIRECTION:
-      if (scan_data_[CENTER] > check_forward_dist_)
-      {
-        if (scan_data_[LEFT] < check_side_dist_)
-        {
-          //Turn right if there is something in front and there is something on the right
-          prev_tb3_pose_ = tb3_pose_;
-          turtlebot3_state_num = TB3_RIGHT_TURN;
-        }
-        else if (scan_data_[RIGHT] < check_side_dist_)
-        {
-          //Turn left if there is nothing in front, and if there is nothing on the left
-          prev_tb3_pose_ = tb3_pose_;
-          turtlebot3_state_num = TB3_LEFT_TURN;
-        }
-        else
-        {
-          turtlebot3_state_num = TB3_DRIVE_FORWARD;
-        }
-      }
-
-      if (scan_data_[CENTER] < check_forward_dist_)
-      {
-        prev_tb3_pose_ = tb3_pose_;
+      //Check if the robot needs to make a right turn
+      if(Laser.scan_data_[CENTER] >check_forward_dist_ && Laser.scan_data_[RIGHT_300] > check_side_dist_ && Laser.scan_data_[RIGHT_285]> check_side_dist_ && Laser.scan_data_[RIGHT_330] > check_side_dist_)     {  
+        //Update prev pose  
+        Odom.prev_tb3_pose_ = Odom.tb3_pose_;
+        //Change state to right turn
         turtlebot3_state_num = TB3_RIGHT_TURN;
       }
+      //Check if the robot needs to go forward
+      else if(Laser.scan_data_[CENTER]>check_forward_dist_ ){
+        //Update prev pose  
+        Odom.prev_tb3_pose_ = Odom.tb3_pose_;
+        //Change state to forward
+        turtlebot3_state_num = TB3_DRIVE_FORWARD;
+      }
+      //Check if the robot needs to make a left turn
+      else if (Laser.scan_data_[CENTER] < check_forward_dist_ || Laser.scan_data_[RIGHT_270]<check_side_dist_ || Laser.scan_data_[RIGHT_345]<check_side_dist_ || Laser.scan_data_[RIGHT_300]<check_side_dist_)  {
+        //Update prev pose  
+        Odom.prev_tb3_pose_ = Odom.tb3_pose_;
+        //Change state to left turn
+        turtlebot3_state_num = TB3_LEFT_TURN;
+      }
       break;
-
+ 
+    //Robot drives forward
     case TB3_DRIVE_FORWARD:
       updatecommandVelocity(LINEAR_VELOCITY, 0.0);
       turtlebot3_state_num = GET_TB3_DIRECTION;
       break;
-    
+
+    //Robot makes a right turn
     case TB3_RIGHT_TURN:
-      if (fabs(prev_tb3_pose_ - tb3_pose_) >= escape_range_)
+      if (fabs(Odom.prev_tb3_pose_ - Odom.tb3_pose_) >= escape_range_)
         turtlebot3_state_num = GET_TB3_DIRECTION;
       else
         updatecommandVelocity(0.0, -1 * ANGULAR_VELOCITY);
       break;
-    
+
+    //Robot makes a left turn
     case TB3_LEFT_TURN:
-      if (fabs(prev_tb3_pose_ - tb3_pose_) >= escape_range_)
+      if (fabs(Odom.prev_tb3_pose_ - Odom.tb3_pose_) >= escape_range_)
         turtlebot3_state_num = GET_TB3_DIRECTION;
       else
         updatecommandVelocity(0.0, ANGULAR_VELOCITY);
@@ -164,7 +181,7 @@ bool Turtlebot3Drive::controlLoop()
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "turtlebot3_drive");
-  Turtlebot3Drive turtlebot3_drive;
+  Turtlebot3 turtlebot3_drive;
 
   ros::Rate loop_rate(125);
 
